639ef99e7a7e1f22353c078d63bb527faafb73b9
krvsc <vskumar93@outlook.com>
2024-05-04 20:58:04 +0530

    added few instructions

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
+# Put the value of ami_id, instance_name directly or choose to put in .tfvar file 
 #Dev infra
-
 module "dev-demo-app" {
     source = "./module/demo-app"
     env_name = "Dev"
@@ -11,9 +11,25 @@ module "dev-demo-app" {
 }
 
 
-
-#QA infra
-
+#Staging infra
+module "Stg-demo-app" {
+    source = "./module/demo-app"
+    env_name = "Stg"
+    instance_name = "pqr"
+    instance_type = "t2.small"
+    ami_id = ""
+    bucket_name = "s3"
+    table_name = "Stg-table"
+}
 
 
 #Prod infra
+module "Prod-demo-app" {
+    source = "./module/demo-app"
+    env_name = "Prod"
+    instance_name = "xyz"
+    instance_type = "t2.medium"
+    ami_id = ""
+    bucket_name = "s3"
+    table_name = "Prod-table"
+}
